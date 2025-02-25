7684a94cc4b02f87cc96fe3b6c0425fe393544f1
Daradev1 <lekissdarams7@gmail.com>
2025-02-25 11:45:37 -0800

    sdara

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -4,9 +4,9 @@ import { FaEnvelope, FaPhone, FaMapMarker } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="bg-pink-600 text-white py-4">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-sm mb-2">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-        <div className="flex justify-center space-x-4">
+      <div className=" mx-auto px-4 text-center">
+        <p className="text-sm mb-2">&copy; {new Date().getFullYear()} DietPreg. All rights reserved.</p>
+        <div className="flex md:flex-row flex-col justify-center items-center space-x-4">
           <div className="flex items-center">
             <FaEnvelope className="mr-2" />
             <span>info@example.com</span>
